3ac408c98fe207ae5d92a845c160d34b7464de7b
DenisLaptev <laptev.denis@mail.ru>
2017-04-15 14:55:04 +0300

    init

---
+++ app/src/main/java/com/sourceit/sourceit1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.sourceit.sourceit1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textView;
+    Button btnPrint;
+    Button btnClear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        btnPrint = (Button) findViewById(R.id.btnPrint);
+        btnPrint.setOnClickListener(this);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnPrint:
+                textView.setText("Hello, World!");
+                break;
+
+            case R.id.btnClear:
+                textView.setText("");
+                break;
+        }
+    }
+}
